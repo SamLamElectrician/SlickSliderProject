031a019ab2b526f28a31a59de03353005627deb6
Sammy <sammylam_1@hotmail.com>
2023-01-20 23:03:16 -0500

    working horizontal slider plus observer

--- script.js
+++ script.js
@@ -6,28 +6,58 @@ app.buttonModules = () => {
 	const mainModule = document.getElementById('mainModule');
 	const exitModule = document.getElementById('exitModule');
 	const slideshow = document.getElementById('slideshowContainer');
-	const mainImg = document.getElementById('mainImg');
 	const doneButton = document.getElementById('complete');
+	const mainImg = document.getElementById('mainImg');
+	const body = document.body;
 
-	//toggles slideshow
+	const innerSlider = document.getElementById('innerSlider');
+	const slides = document.querySelectorAll('.slide');
+
+	const scroll = () => {
+		const scrollContainer = document.querySelector('.slider');
+
+		scrollContainer.addEventListener('wheel', (e) => {
+			e.preventDefault();
+			scrollContainer.scrollLeft += e.deltaY;
+		});
+	};
+	//toggles slideshow on and removes background image
 	viewButton.addEventListener('click', () => {
 		mainModule.classList.toggle('hidden');
 		exitModule.classList.toggle('visible');
 		slideshow.classList.toggle('visible');
-		mainImg.classList.toggle('hidden');
+		body.style.backgroundImage = 'none';
+		body.classList.toggle('grey');
+		scroll();
+		// mainImg.classList.toggle('hidden');
 	});
-	//toggle main image again
+
+	//removes the slider and goes to main feature
 	doneButton.addEventListener('click', () => {
 		mainModule.classList.toggle('hidden');
 		exitModule.classList.toggle('visible');
 		slideshow.classList.toggle('visible');
-		mainImg.classList.toggle('hidden');
+		body.classList.toggle('grey');
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach(
+				(entry) => {
+					if (entry.isIntersecting) {
+						body.style.backgroundImage = `url(${entry.target.firstElementChild.attributes[0].nodeValue})`;
+					}
+				},
+				{ threshold: 1 }
+			);
+		});
+
+		slides.forEach((child) => {
+			observer.observe(child);
+		});
 	});
 };
 
 app.init = () => {
 	app.buttonModules();
-	app.slider();
+
 	// app.slideshow();
 };
 
