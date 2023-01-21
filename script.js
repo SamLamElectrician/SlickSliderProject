b6f049be7a7f42d85a1de1c5d43c512fb4c2b93c
Sammy <sammylam_1@hotmail.com>
2023-01-21 18:45:56 -0500

    created a duplicate feature

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 let app = {};
 
 app.buttonModules = () => {
+	// all variables that are needed to hide or show full screen mode
 	const viewButton = document.getElementById('viewButton');
-	const changeRoom = document.getElementById('changeRoom');
+	const changeButton = document.getElementById('changeRoom');
+	const exitButton = document.getElementById('mainExitButton');
 	const mainModule = document.getElementById('mainModule');
 	const exitModule = document.getElementById('exitModule');
 	const slideshow = document.getElementById('slideshowContainer');
 	const doneButton = document.getElementById('complete');
-	const mainImg = document.getElementById('mainImg');
 	const body = document.body;
 
-	const innerSlider = document.getElementById('innerSlider');
-	const slides = document.querySelectorAll('.slide');
+	//button module array to have them all do the same thing since I thought they were roughly similiar
+	const buttonArray = [viewButton, changeButton, exitButton];
 
+	//selecting all the slides
+	const slides = document.querySelectorAll('.slide');
+	//empty string which changes depending on what the observer is viewing
+	let img = '';
+	//observer looks for what is currently in the viewport
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach(
+			(entry) => {
+				if (entry.isIntersecting) {
+					// body.style.backgroundImage;
+					img = `url(${entry.target.firstElementChild.attributes[0].nodeValue})`;
+				}
+			},
+			{ threshold: 1, rootMargin: '-200px' }
+		);
+	});
+	//attaches an observer to each slide to indicate whether it is in the viewport
+	slides.forEach((child) => {
+		observer.observe(child);
+	});
+
+	//scroll function to move the slideshow
 	const scroll = () => {
 		const scrollContainer = document.querySelector('.slider');
-
 		scrollContainer.addEventListener('wheel', (e) => {
 			e.preventDefault();
-			scrollContainer.scrollLeft += e.deltaY;
+			scrollContainer.scrollLeft += e.deltaY / 2;
 		});
 	};
-	//toggles slideshow on and removes background image
-	viewButton.addEventListener('click', () => {
-		mainModule.classList.toggle('hidden');
-		exitModule.classList.toggle('visible');
-		slideshow.classList.toggle('visible');
-		body.style.backgroundImage = 'none';
-		body.classList.toggle('grey');
-		scroll();
-		// mainImg.classList.toggle('hidden');
+	//buttons to hide the main image and show the slide show
+	buttonArray.forEach((button) => {
+		button.addEventListener('click', () => {
+			mainModule.classList.toggle('hidden');
+			exitModule.classList.toggle('visible');
+			slideshow.classList.toggle('visible');
+			body.style.backgroundImage = 'none';
+			body.classList.toggle('grey');
+			scroll();
+		});
 	});
 
 	//removes the slider and goes to main feature
@@ -38,25 +61,52 @@ app.buttonModules = () => {
 		exitModule.classList.toggle('visible');
 		slideshow.classList.toggle('visible');
 		body.classList.toggle('grey');
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach(
-				(entry) => {
-					if (entry.isIntersecting) {
-						body.style.backgroundImage = `url(${entry.target.firstElementChild.attributes[0].nodeValue})`;
-					}
-				},
-				{ threshold: 1 }
-			);
-		});
+		body.style.backgroundImage = `${img}`;
+	});
+};
 
-		slides.forEach((child) => {
-			observer.observe(child);
+//function to duplicate a slide and add another one on to it
+app.duplicateSlide = () => {
+	const duplicate = document.querySelectorAll('.duplicate');
+	const slideshow = document.querySelector('.innerSlider');
+	duplicate.forEach((double) => {
+		double.addEventListener('click', (e) => {
+			const imageSRC =
+				double.parentNode.parentElement.parentElement.firstElementChild
+					.attributes[0].nodeValue;
+			const heading =
+				double.parentNode.parentElement.firstElementChild.children[0].innerText;
+			const paragraph =
+				double.parentNode.parentElement.firstElementChild.children[1].innerText;
+			const newDuplicate = document.createElement('div');
+			newDuplicate.className = 'slide';
+			newDuplicate.innerHTML = `
+						<img
+							src="${imageSRC}"
+							alt="yellow Chair"
+							draggable="false"
+						/>
+						<div class="infoCard">
+							<div class="info">
+								<h1>${heading}</h1>
+								<p>${paragraph}</p>
+							</div>
+							<div class="infoButtons">
+								<button>Share</button>
+								<button>Favorite</button>
+								<button class="duplicate">Duplicate</button>
+							</div>
+						</div>
+					`;
+			slideshow.appendChild(newDuplicate);
+			alert('Your Slide is added');
 		});
 	});
 };
 
 app.init = () => {
 	app.buttonModules();
+	app.duplicateSlide();
 
 	// app.slideshow();
 };
